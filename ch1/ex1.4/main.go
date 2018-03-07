a2a3313f1d5e81ea808fa41cc5ad5a921e2d0b8b
takatori <takatorix@gmail.com>
2018-03-08 01:24:10 +0900

    Add ex1.4

---
+++ ch1/ex1.4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	occurrences := make(map[string][]string)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, occurrences)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup:2 %v\n", err)
+				continue
+			}
+			countLines(f, counts, occurrences)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%v\n", n, line, occurrences[line])
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, occurrences map[string][]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+		occurrences[input.Text()] = append(occurrences[input.Text()], f.Name())
+	}
+}
