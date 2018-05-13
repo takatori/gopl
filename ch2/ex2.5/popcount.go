5c059df4f0bbc0a932f119849089d285b28f4132
takatori <takatorix@gmail.com>
2018-05-13 18:27:09 +0900

    add ex2.5

---
+++ ch2/ex2.5/popcount.go
@@ -0,0 +1,36 @@
+package popcount
+
+// pc[i]はiのポピュレーションカウント
+var pc [256]byte
+
+// プログラムが開始した時点で宣言されてした順序で自動的に実行される
+// 呼び出すことも参照することもできない
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount はXのポピュレーションカウントを返す
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// PopCount はXのポピュレーションカウントを返す
+func PopCountBitClear(x uint64) int {
+
+	var num int
+
+	for ; x != 0; x = x & (x - 1) {
+		num++
+	}
+
+	return num
+}
